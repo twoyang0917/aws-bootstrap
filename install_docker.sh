82f90df76813b6a03eeb1bd0e22ba18752093c30
twoyang0917 <twoyang_0917@qq.com>
2018-12-06 10:44:13 +0800

    add docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -2,3 +2,6 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 apt-get update && apt-get install -y docker-ce
+
+curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
