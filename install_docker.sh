9f608f0c37dee5e3a638886d1ebd09b490a6958c
twoyang0917 <twoyang_0917@qq.com>
2018-12-06 10:47:01 +0800

    add tutum/hello-world

--- install_docker.sh
+++ install_docker.sh
@@ -5,3 +5,7 @@ apt-get update && apt-get install -y docker-ce
 
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+mkdir -p /services && cd /services
+cp -f /tmp/aws-cfn-scripts/docker-compose.yml /services/
+docker-compose up -d
