b7579481dfdea9aae230b219ccb275062fda212d
twoyang0917 <twoyang_0917@qq.com>
2018-12-07 11:15:36 +0800

    move env file out of docker-compose.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,5 @@ apt-get update && apt-get install -y docker-ce
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-mkdir -p /services && cd /services
-cp -f /tmp/aws-bootstrap/docker-compose.yml /services/
+cp -f /services/aws-bootstrap/docker-compose.yml /services/
 docker-compose up -d
