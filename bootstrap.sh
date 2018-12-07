9739e91965f9bd994ad5196892f4ebc7a97fb281
twoyang0917 <twoyang_0917@qq.com>
2018-12-07 11:25:56 +0800

    fix issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,9 @@ apt-get update && apt-get install -y docker-ce
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+pip install awscli
+
+DBPassword="$(aws ssm get-parameter --name /CloudFormation/Wordpress/DBPassword --with-decryption)"
+sed -i "s/changeit/${DBPassword}/" /services/.env
 cp -f /services/aws-bootstrap/docker-compose.yml /services/
 docker-compose up -d
