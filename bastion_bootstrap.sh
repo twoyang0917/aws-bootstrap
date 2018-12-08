24b310895744c390d8c4104f3da88b054f986cf0
twoyang0917 <twoyang_0917@qq.com>
2018-12-08 20:10:49 +0800

    add ansible as configuration management

--- bastion_bootstrap.sh
+++ bastion_bootstrap.sh
@@ -5,3 +5,14 @@ apt-get update && apt-get install -y docker-ce
 
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+pip install awscli boto3
+
+apt-add-repository --yes ppa:ansible/ansible
+apt-get update && apt-get install -y ansible
+
+cd /services
+cp -f aws-bootstrap/ansible/ec2.py /etc/ansible/ && chmod +x /etc/ansible/ec2.py
+cp -f aws-bootstrap/ansible/ec2.ini /etc/ansible/
+cp -f aws-bootstrap/ansible/ansible.cfg /etc/ansible/
+python /etc/ansible/ec2.py
