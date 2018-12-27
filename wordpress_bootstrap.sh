ac2b1e84872ac11e348c42b6e4e1f2b7dc34ab0f
twoyang0917 <twoyang_0917@qq.com>
2018-12-27 14:32:44 +0800

    install stress for autoscaling test

--- wordpress_bootstrap.sh
+++ wordpress_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-apt-get update && apt-get install -y docker-ce
+apt-get update && apt-get install -y docker-ce stress
 
 COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
 curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
